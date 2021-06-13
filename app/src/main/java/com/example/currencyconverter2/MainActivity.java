d94daf62f13a4460f813dcbe900e64887f0a38f2
MJLord <69679203+MJLord@users.noreply.github.com>
2021-06-13 19:46:20 +0530

    Running app

--- app/src/main/java/com/example/currencyconverter2/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter2/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    public static ArrayList<String> list = new ArrayList<String>();
+    public static ArrayList<String> list = new ArrayList<>();
     private static final String TAG = "MainActivity";
     public Spinner List1;
     public Spinner List2;
@@ -51,16 +51,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         new Thread(runnable).start();
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, strList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, strList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         List1.setAdapter(adapter);
+        int spinnerPosition1 = adapter.getPosition("USD");
+        List1.setSelection(spinnerPosition1);
         List1.setOnItemSelectedListener(this);
         List2.setAdapter(adapter);
+        int spinnerPosition2 = adapter.getPosition("INR");
+        List2.setSelection(spinnerPosition2);
         List2.setOnItemSelectedListener(this);
 
     }
@@ -75,7 +79,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Curr1 = List1.getSelectedItem().toString();
         Curr2 = List2.getSelectedItem().toString();
-//        System.out.println(Curr1 + " " + Curr2 + "______________________________________---------------------------------");
     }
 
     @Override
@@ -87,8 +90,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         @Override
         public void run() {
             String conversion = Curr1 + "_" + Curr2;
-            double rate;
-            double result;
+            Double rate;
+            Double result;
             String url = "https://free.currconv.com/api/v7/convert?q="+ conversion +"&compact=ultra&apiKey=92139c44c58c2a36a2d3";
             try {
                 URL rateAPI = new URL(url);
@@ -97,9 +100,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 BufferedReader rateInput = new BufferedReader(new InputStreamReader(rateInputStream));
                 String rateString = rateInput.readLine();
                 JSONObject rateJO = new JSONObject(rateString);
-                rate = (double) rateJO.get(conversion);
-
-                result = rate * Double.parseDouble(String.valueOf(Number));
+                rate = Double.parseDouble(rateJO.get(conversion).toString());
+                result = rate * Double.parseDouble(Number.getText().toString());
                 Result.setText(String.valueOf(result));
             } catch (MalformedURLException e) {
                 e.printStackTrace();
